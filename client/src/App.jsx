a95e790856d7a665b6545326f28f65d8a867fcf2
Ujjwal-Patel-02 <157982245+Ujjwal-Patel-02@users.noreply.github.com>
2024-06-30 18:55:57 +0530

    Header Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route path='/about' element={<About/>}></Route>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,14 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    flowbite.plugin(),
+  ],
+};
