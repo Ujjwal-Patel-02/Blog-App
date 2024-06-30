779a70785d8b0630b640df00fa4719293277d26e
Ujjwal-Patel-02 <157982245+Ujjwal-Patel-02@users.noreply.github.com>
2024-06-30 20:56:27 +0530

    password encrypted

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,24 @@
+import user from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+
+export const register = async (req,res) =>{
+  const {username, email,password} = req.body;
+  if (!username || !email || !password || username === '' || email === '' || password === '') {
+    return res.status(400).json({message: 'All fields are required'}) ;
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password,10);
+
+  const newUser = new user({
+    username,
+    email,
+    password: hashedPassword
+  });
+
+  try{
+    await newUser.save();
+  }catch(error){
+    return res.status(500).json({message: error.message});
+  }
+}
