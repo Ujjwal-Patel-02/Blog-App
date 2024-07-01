0e131d40b02f28ab5e1b2ed79994a3110b9c7cb8
Ujjwal-Patel-02 <157982245+Ujjwal-Patel-02@users.noreply.github.com>
2024-07-01 16:03:13 +0530

    Making server again

--- api/index.js
+++ api/index.js
@@ -1,38 +1,7 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js'
-
-
-dotenv.config()
-
-
-mongoose.connect(process.env.MONGO).then(() =>{
-  console.log("Connected")
-})
-.catch((err) =>{
-  console.log(err);
-})
-
+import express from "express";
 
 const app = express();
 
-app.use(express.json());
-
-app.listen(3000, () =>{
-  console.log('Server is running on port 3000!');
-})
-
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
-
-app.use((err,req,ress,next) =>{
-  const statusCode = err.statusCode||500;
-  const message = err.message|| 'Internal Server Error';
-  ress.statusCode({
-    statusCode,
-    message,
-    success: false
-  })
+app.listen(3000,() =>{
+  console.log("Server is running on port 3000");
 })
--- api/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import {test} from '../controllers/user.controller.js';
-
-const router = express();
-
-router.get('/test', test);
-
-export default router;
